a331b00dfc86c76012cb3e34a94439a991a983f6
Michael Kochis(michael.kochis) <michael.kochis@infosys.com>
2024-12-12 15:48:36 -0600

    Initial Commit

---
+++ src/main/java/com/infosys/cevaDemo/CevaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.cevaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CevaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CevaDemoApplication.class, args);
+	}
+
+}
