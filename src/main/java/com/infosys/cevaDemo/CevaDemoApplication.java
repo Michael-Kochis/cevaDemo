74f883ae0f2e1ab34ff01da46a0cafb0329219ef
Michael Kochis(michael.kochis) <michael.kochis@infosys.com>
2024-12-12 16:54:33 -0600

    Program still not in working state, MojoExceptionError.

--- src/main/java/com/infosys/cevaDemo/CevaDemoApplication.java
+++ src/main/java/com/infosys/cevaDemo/CevaDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CevaDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CevaDemoApplication.class, args);
 	}
 
